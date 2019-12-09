dfa81570a3451e848e72d817b650dff8db0f9739
kiaraBot <afield@cnm.edu>
2019-12-09 16:43:22 -0700

    Layout Commit
    Layout Includes 1, 2, & 3
    App Theme Colors Updated
    NewBoardFragment.xml Completed
    6 Fab Btns Added w/ Icons, will connect to their respective Activites

--- app/src/main/java/com/example/awritersmood/ui/newboard/NewBoardFragment.java
+++ app/src/main/java/com/example/awritersmood/ui/newboard/NewBoardFragment.java
@@ -1,24 +1,19 @@
 package com.example.awritersmood.ui.newboard;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import com.example.awritersmood.R;
+
 
 public class NewBoardFragment extends Fragment {
     
     private NewBoardViewModel newBoardViewModel;
-    
+
 /*    public static NewBoardFragment newInstance() {
         return new NewBoardFragment();
     }*/
@@ -29,13 +24,9 @@ public class NewBoardFragment extends Fragment {
         newBoardViewModel = ViewModelProviders.of(this).get(NewBoardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_newboard, container, false);
         
-/*        final TextView textView = root.findViewById(R.id.text_newboard);
-        newBoardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                textView.setText(s);
-            }
-        });*/
+
+
         return root;
     }
+
 }
